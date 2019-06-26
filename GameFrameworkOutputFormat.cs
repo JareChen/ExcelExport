8418170738eae661a78fb3ebc3d989c277c2d1cb
JareChen <JareChen@outlook.com>
2019-06-26 16:43:29 +0800

    配置工具修改，适配 UNO 项目

--- GameFrameworkOutputFormat.cs
+++ GameFrameworkOutputFormat.cs
@@ -27,7 +27,7 @@ public class GameFrameworkOutputFormat : OutputFormat
         StringBuilder outputCS = new StringBuilder();
 
         StringBuilder csClassFormat = new StringBuilder();
-        csClassFormat.AppendLine("public class {0} : IDataRow");
+        csClassFormat.AppendLine("public class {0} : Config");
         csClassFormat.AppendLine("{{");
         csClassFormat.AppendLine("{1}");
         csClassFormat.AppendLine("}}");
@@ -43,13 +43,43 @@ public class GameFrameworkOutputFormat : OutputFormat
         csFieldFormat.AppendLine("\t}}");
 
         ExcelRange classInfo = workSheetItem.Cells[4, columnCount];
-        for (int i = 1; i <= rowCount; i++)
+        for (int i = 4; i <= rowCount; i++)
         {
             for (int j = 1; j <= columnCount; j++)
             {
-                var cellValue = workSheetItem.GetValue(i, j);
-                string value = cellValue == null ? string.Empty : cellValue.ToString(); 
-                outputTxt.Append(value + "\t");
+                if (i > 4)
+                {
+                    var cellValue = workSheetItem.GetValue(i, j);
+                    string value = string.Empty;
+                    if (cellValue == null)
+                    {
+                        if (classInfo[3, j].GetValue<string>() == "int")
+                        {
+                            value = "0";
+                        }
+                    }
+                    else
+                    {
+                        value = cellValue.ToString();
+                    }
+                     
+                    if (j < columnCount)
+                    {
+                        outputTxt.Append(value + "\t");
+                    }
+                    if (j == columnCount)
+                    {
+                        if (i <= rowCount)
+                        {
+                            outputTxt.Append(value);
+                        }
+                        
+                        if (i < rowCount)
+                        {
+                            outputTxt.AppendLine();
+                        }
+                    }
+                }
 
                 if (i == 4 && j > 1)
                 {
@@ -59,32 +89,31 @@ public class GameFrameworkOutputFormat : OutputFormat
                     outputCS.AppendLine(string.Format(csFieldFormat.ToString(), expositionStr, typeStr, fieldNameStr));
                 }
             }
-            outputTxt.AppendLine();
         }
 
         StringBuilder parseDataRowMethodFormat = new StringBuilder();
-        parseDataRowMethodFormat.AppendLine("\tpublic void ParseDataRow(string dataRowText)");
+        parseDataRowMethodFormat.AppendLine("\tpublic override void ParseDataRow(string dataRowText)");
         parseDataRowMethodFormat.AppendLine("\t{");
-        parseDataRowMethodFormat.AppendLine("\t\tstring[] text = DataTableExtension.SplitDataRow(dataRowText);");
-        parseDataRowMethodFormat.AppendLine("\t\tint index = 1;");
-        for (int j = 2; j <= columnCount; j++)
+        parseDataRowMethodFormat.AppendLine("\t\tstring[] text = dataRowText.Split('\\t');");
+        parseDataRowMethodFormat.AppendLine("\t\tint index = 0;");
+        for (int j = 1; j <= columnCount; j++)
         {
             string typeStr = classInfo[3, j].GetValue<string>();
             string fieldNameStr = classInfo[4, j].GetValue<string>();
             if (typeStr == "int")
             {
-                parseDataRowMethodFormat.AppendLine(string.Format("\t\t{0} = int.Parse(text[++index]);", fieldNameStr));
+                parseDataRowMethodFormat.AppendLine(string.Format("\t\t{0} = int.Parse(text[index++]);", fieldNameStr));
             }
             else
             {
-                parseDataRowMethodFormat.AppendLine(string.Format("\t\t{0} = text[++index];", fieldNameStr));
+                parseDataRowMethodFormat.AppendLine(string.Format("\t\t{0} = text[index++];", fieldNameStr));
             }
         }
         parseDataRowMethodFormat.Append("\t}");
         outputCS.Append(parseDataRowMethodFormat.ToString());
         outputTxtStr = outputTxt.ToString(); 
         outputCSStr = string.Format(csClassFormat.ToString(), fileNameWithoutExt, outputCS);
-
+        
         string txtFileName = fileNameWithoutExt + ".txt";
         OutputFile(txtFileName, outputTxtStr);
 
